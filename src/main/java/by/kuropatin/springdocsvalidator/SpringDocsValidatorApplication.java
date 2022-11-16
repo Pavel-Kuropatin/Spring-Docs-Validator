c84d7da1e95c91199409b7d19b90d03853c6ec78
Pavel Kuropatin <kuropatin.pv@gmail.com>
2022-11-16 22:12:45 +0300

    Initial commit

---
+++ src/main/java/by/kuropatin/springdocsvalidator/SpringDocsValidatorApplication.java
@@ -0,0 +1,13 @@
+package by.kuropatin.springdocsvalidator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDocsValidatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringDocsValidatorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/by/kuropatin/springdocsvalidator/SpringDocsValidatorApplicationTests.java
@@ -0,0 +1,13 @@
+package by.kuropatin.springdocsvalidator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDocsValidatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+
+    }
+}
